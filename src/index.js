71ab219c0c88babb78fbdc84fef5e4f13a301b9f
Sinisa Damjanovic <sikidamjanovic@gmail.com>
2019-01-22 18:11:40 -0500

    Fixed bootstrap and dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, browserHistory} from 'react-router'
 import Routes from './routes'
-import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
